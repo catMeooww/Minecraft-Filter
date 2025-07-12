eaa1853198ced0b248c7ab857429d49a612f411f
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2025-07-11 23:36:11 -0300

    General Change

--- sketch.js
+++ sketch.js
@@ -27,11 +27,14 @@ function preload() {
   hotbar = loadImage("survival.png");
 }
 
+wwidth = window.screen.width - 11;
+wheight = window.screen.height - 175;
+
 function setup() {
-  canvas = createCanvas(400, 400);
+  canvas = createCanvas(wwidth,wheight);
   canvas.parent("canvas")
   video = createCapture(VIDEO);
-  video.size(400, 400);
+  video.size(wwidth, wheight);
   video.hide();
 
   poseNet = ml5.poseNet(video, modelLoaded);
@@ -40,55 +43,55 @@ function setup() {
 
 function draw() {
   background(220);
-  image(video, 0, 0, 400, 400);
+  image(video, 0, 0, wwidth, wheight);
   fill(255, 0, 0);
   stroke(255, 0, 0);
   if (selected == 1) {
-    image(creeper, noseX - 50, noseY - 60, 90, 90);
+    image(creeper, noseX - 95, noseY - 105, 190, 190);
   } else if (selected == 2) {
-    image(enderman, noseX - 50, noseY - 60, 90, 90);
+    image(enderman, noseX - 95, noseY - 105, 190, 190);
   } else if (selected == 3) {
-    image(steve, noseX - 50, noseY - 60, 90, 90);
-    image(picareta, noseX - 100, noseY + 150, 70, 70);
-    image(diamante, noseX + 100, noseY + 150, 60, 60);
+    image(steve, noseX - 95, noseY - 105, 190, 190);
+    image(picareta, noseX - 100, noseY + 180, 140, 140);
+    image(diamante, noseX + 100, noseY + 180, 120, 120);
   } else if (selected == 4) {
-    image(steve, noseX - 50, noseY - 60, 90, 90);
-    image(espada, noseX - 100, noseY + 150, 70, 70);
-    image(arco, noseX + 100, noseY + 150, 60, 60);
+    image(steve, noseX - 95, noseY - 105, 190, 190);
+    image(espada, noseX - 100, noseY + 180, 140, 140);
+    image(arco, noseX + 100, noseY + 180, 120, 120);
   } else if (selected == 5) {
-    image(pig, noseX - 50, noseY - 60, 90, 90);
+    image(pig, noseX - 95, noseY - 105, 190, 190);
   } else if (selected == 6) {
-    image(steve, noseX - 50, noseY - 60, 90, 90);
-    image(creative, noseX - 100, noseY + 150, 200, 90);
+    image(steve, noseX - 95, noseY - 105, 190, 190);
+    image(creative, noseX - 150, noseY + 150, 280, 150);
   } else if (selected == 7) {
-    image(axolote, noseX - 55, noseY - 100, 150, 145);
+    image(axolote, noseX - 155, noseY - 190, 290, 290);
   } else if (selected == 8) {
-    image(esqueleto, noseX - 50, noseY - 60, 90, 90);
-    image(arco, noseX + 100, noseY + 150, 60, 60);
+    image(esqueleto, noseX - 95, noseY - 105, 190, 190);
+    image(arco, noseX + 100, noseY + 180, 120, 120);
   } else if (selected == 9) {
-    image(aranha, noseX - 50, noseY - 60, 90, 90);
-    image(teia, noseX + 30, noseY + 150, 80, 80);
-    image(teia, noseX + 110, noseY + 157, 80, 80);
-    image(teia, noseX - 30, noseY + 160, 80, 80);
-    image(teia, noseX - 110, noseY + 150, 80, 80);
-    image(teia, noseX - 150, noseY + 170, 80, 80);
+    image(aranha, noseX - 95, noseY - 105, 190, 190);
+    image(teia, noseX + 40, noseY + 180, 160, 160);
+    image(teia, noseX + 120, noseY + 187, 160, 160);
+    image(teia, noseX - 40, noseY + 190, 160, 160);
+    image(teia, noseX - 120, noseY + 170, 160, 160);
+    image(teia, noseX - 160, noseY + 190, 160, 160);
   } else if (selected == 10) {
-    image(zumbi, noseX - 50, noseY - 60, 90, 90);
+    image(zumbi, noseX - 95, noseY - 105, 190, 190);
   } else if (selected == 11) {
-    image(warden, noseX - 50, noseY - 60, 90, 90);
-    image(wardenChest, noseX - 50, noseY + 130, 100, 100);
+    image(warden, noseX - 95, noseY - 105, 190, 190);
+    image(wardenChest, noseX - 95, noseY + 150, 200, 200);
   } else if (selected == 12) {
-    image(villager, noseX - 50, noseY - 60, 90, 90);
-    image(esmerald, noseX - 100, noseY + 150, 70, 70);
-    image(esmerald, noseX + 100, noseY + 150, 60, 60);
+    image(villager, noseX - 95, noseY - 105, 190, 190);
+    image(esmerald, noseX - 100, noseY + 180, 140, 140);
+    image(esmerald, noseX + 100, noseY + 180, 120, 120);
   } else if (selected == 13) {
-    image(wolf, noseX - 50, noseY - 60, 100, 90);
+    image(wolf, noseX - 200, noseY - 125, 390, 290);
   } else if (selected == 14) {
-    image(steve, noseX - 50, noseY - 60, 90, 90);
-    image(hotbar, noseX - 100, noseY + 90, 200, 170);
+    image(steve, noseX - 95, noseY - 105, 190, 190);
+    image(hotbar, noseX - 150, noseY + 150, 270, 240);
   }else if (selected == 15){
-    image(blazebody, noseX - 50, noseY - 60, 90, 250);
-    image(blaze, noseX - 50, noseY - 60, 90, 90);
+    image(blazebody, noseX - 95, noseY, 190, 280);
+    image(blaze, noseX - 95, noseY - 105, 190, 190);
   }
 }
 
@@ -110,6 +113,18 @@ function gotPoses(results) {
 function Select(mob) {
   selected = mob;
   console.log(mob);
+}
+
+function toggleMenu(){
+  if(document.getElementById("options").style.visibility == "hidden"){
+    document.getElementById("options").style.visibility = "visible";
+    document.getElementById("options").style.height = (wheight/2-50)+"px";
+    document.getElementById("collapser").innerHTML = "^";
+  }else{
+    document.getElementById("options").style.visibility = "hidden";
+    document.getElementById("options").style.height = "0px";
+    document.getElementById("collapser").innerHTML = "=";
+  }
 }
 
 function MoreSite(){
